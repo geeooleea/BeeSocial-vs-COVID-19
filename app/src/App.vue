4ed1c0f433b123baf6e03e0c5751548ede2d01d2
Matteo Padovan <mpadovan@DESKTOP-1LC3J28.localdomain>
2020-03-30 15:18:07 +0200

    technical commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <router-link to="/"></router-link>
     <router-link to="/2"></router-link>
+    <router-link to="/chat"></router-link>
     <router-view/>
   </v-app>
 </template>
--- app/src/components/ActivityList.vue
+++ app/src/components/ActivityList.vue
@@ -1,9 +1,18 @@
 <template>
   <v-container fluid class="padd">
     <v-row dense>
-      <v-card class="mx-auto paddc" max-width="350" color="#26c6da" dark v-for="activity in activities" :key="activity.message">
+      <v-card
+        class="mx-auto paddc"
+        max-width="350"
+        color="#26c6da"
+        dark
+        v-for="activity in activities"
+        :key="activity.message"
+        v-on:click="pa(activity.id)"
+        @click.stop="dialog = true"
+      >
         <v-img
-          :src=activity.img
+          :src="activity.img"
           class="white--text align-end"
           gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
           height="200px"
@@ -12,31 +21,70 @@
         </v-img>
       </v-card>
     </v-row>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">And the winner is...</v-card-title>
+        <div class="text-center">
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </div>
+        <v-card-text>
+          <v-spacer></v-spacer>
+          <p
+            class="subtitle-1"
+          >...just like Oscars you have to wait a bit, but don't worry a buddy is right around the corner</p>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import data from "../data.js";
 
-export default {
+let a = {
   name: "ActivityList",
 
   data() {
     return {
-      activities: null
-    }
+      activities: null,
+      polling: null,
+      dialog: null
+    };
   },
 
   mounted() {
-    axios.get('http://localhost:3000/activities')
-    .then((res) => {
+    axios.get("http://localhost:3000/activities").then(res => {
       this.activities = res.data;
-    })
+    });
   },
 
   methods: {
+    pa(a) {
+      axios
+        .post("http://localhost:3000/shared_activity", {
+          id: a,
+          user: data.getUser().data.id
+        })
+        .then(res => {
+          this.pollData(res);
+        });
+    },
+    pollData(res) {
+      this.polling = setInterval(() => {
+            axios.get("http://localhost:3000/buddy/" + res.data.id)
+            .then(res => {
+              if (res.data.matched === true) {
+                clearInterval(this.polling)
+                this.$router.push('chat');
+              }
+            });
+          }, 1000);
+    }
   }
 };
+
+export default a;
 </script>
 <style scoped>
 .padd {
