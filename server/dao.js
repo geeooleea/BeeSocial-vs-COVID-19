0ba43fc59f4cd2b3e35616e5380d0b301cb0fbb3
Giulia Carocari <giuliacarocari@gmail.com>
2020-04-10 20:16:24 +0200

    Create DAO class

---
+++ server/dao.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'beesocial',
+    port: process.env.DB_PORT,
+    timeout: 3 * 1000
+});
+
+class Dao {
+    constructor() {
+        this.connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: 'beesocial',
+            port: process.env.DB_PORT,
+            timeout: 3 * 1000
+        });
+    }
+
+    connect() {
+        this.connection.connect();
+    }
+
+    async getActivities(callback) {
+        await this.connection.query('SELECT * FROM activities', function (error, results, fields) {
+            if (error) throw error;
+            return callback(results);
+        });
+    }
+}
+
+module.exports = Dao;
